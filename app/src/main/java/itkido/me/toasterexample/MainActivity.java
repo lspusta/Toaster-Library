ae6c8e3357e8e017dc6d29cc10720da77b97d4c7
Martin Spusta <lucas.spusta@gmail.com>
2020-06-25 09:35:14 -0400

    Added Working Example

--- app/src/main/java/itkido/me/toasterexample/MainActivity.java
+++ app/src/main/java/itkido/me/toasterexample/MainActivity.java
@@ -3,12 +3,25 @@ package itkido.me.toasterexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import itkido.me.toasterlibrary.ToasterMessage;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnDisplayToast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnDisplayToast = findViewById(R.id.btnDisplayToast);
+
+        btnDisplayToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToasterMessage.createToasterMessage(MainActivity.this, "Hello World!");
+            }
+        });
+
     }
 }
